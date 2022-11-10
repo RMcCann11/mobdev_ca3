56151ca532cf35af90147f68b69776c7d910ada2
RMcCann11 <ronanemccann@hotmail.com>
2022-11-10 11:58:10 +0000

    Set entry point to TabsPage component

--- BreakingBadFandom/src/app/app-routing.module.ts
+++ BreakingBadFandom/src/app/app-routing.module.ts
@@ -2,59 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'tabs',
-    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
-  },
-  {
-    path: 'characters',
-    loadChildren: () => import('./pages/characters/characters.module').then( m => m.CharactersPageModule)
-  },
-  {
-    path: 'character-details',
-    loadChildren: () => import('./pages/character-details/character-details.module').then( m => m.CharacterDetailsPageModule)
-  },
-  {
-    path: 'episodes',
-    loadChildren: () => import('./pages/episodes/episodes.module').then( m => m.EpisodesPageModule)
-  },
-  {
-    path: 'episode-details',
-    loadChildren: () => import('./pages/episode-details/episode-details.module').then( m => m.EpisodeDetailsPageModule)
-  },
-  {
-    path: 'quotes',
-    loadChildren: () => import('./pages/quotes/quotes.module').then( m => m.QuotesPageModule)
-  },
-  {
-    path: 'quotes-by-character',
-    loadChildren: () => import('./pages/quotes-by-character/quotes-by-character.module').then( m => m.QuotesByCharacterPageModule)
-  },
-  {
-    path: 'death-count',
-    loadChildren: () => import('./pages/death-count/death-count.module').then( m => m.DeathCountPageModule)
-  },
-  {
-    path: 'death-count-by-character',
-    loadChildren: () => import('./pages/death-count-by-character/death-count-by-character.module').then( m => m.DeathCountByCharacterPageModule)
-  },
-  {
-    path: 'deaths',
-    loadChildren: () => import('./pages/deaths/deaths.module').then( m => m.DeathsPageModule)
-  },
-  {
-    path: 'death-details',
-    loadChildren: () => import('./pages/death-details/death-details.module').then( m => m.DeathDetailsPageModule)
-  },
+  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)}
 ];
 
 @NgModule({
