8ee6e9c1f70f6a7101b42a269e58059daaa0c346
RMcCann11 <ronanemccann@hotmail.com>
2022-11-10 14:56:30 +0000

    Created logic and view for DeathdsPageComponent

--- BreakingBadFandom/src/app/pages/deaths/deaths.page.ts
+++ BreakingBadFandom/src/app/pages/deaths/deaths.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-deaths',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeathsPage implements OnInit {
 
-  constructor() { }
+  characters: Observable<any>;
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.characters = this.http.get('https://www.breakingbadapi.com/api/characters');
+    console.log(this.characters);
+  }
+
+  openDetails(character){
+    let split = character.name.split(' ');
+    let firstName = split[0];
+    let lastName = split[1];
+    this.router.navigate(
+      ['tabs/deaths/death'],
+      { queryParams: { name: firstName + '+' + lastName } }
+  );
   }
 
 }
