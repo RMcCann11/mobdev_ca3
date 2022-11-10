3d795e985f2cfbc0550c36a3374050ba64deb1ca
RMcCann11 <ronanemccann@hotmail.com>
2022-11-10 13:11:39 +0000

    Created logic and view for CharactersPageComponent

--- BreakingBadFandom/src/app/pages/characters/characters.page.ts
+++ BreakingBadFandom/src/app/pages/characters/characters.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-characters',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharactersPage implements OnInit {
 
-  constructor() { }
+  characters: Observable<any>;
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    this.characters = this.http.get('https://www.breakingbadapi.com/api/characters');
+  }
+
+  openDetails(character){
+    let split = character.name.split(' ');
+    let firstName = split[0];
+    let lastName = split[1];
+    this.router.navigate(
+      ['tabs/characters/characters'],
+      { queryParams: { name: firstName + '+' + lastName } }
+    );
   }
 
 }
